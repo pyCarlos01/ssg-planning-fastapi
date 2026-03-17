8d327f05799552cd11890c5c98aaafca896b447f
Carlos Damião <136815475+pyCarlos01@users.noreply.github.com>
2026-03-17 11:02:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ load_dotenv()
 ALGORITHM = os.getenv("ALGORITHM")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+db = create_engine(os.getenv("DATABASE_URL"))
+BASE.metadata.create_all(bind=db)
 
 app = FastAPI()
 
@@ -21,5 +23,3 @@ from man_routes import man_router
 app.include_router(auth_router)
 app.include_router(man_router)
 
-db = create_engine(os.getenv("DATABASE_URL"))
-BASE.metadata.create_all(bind=db)
